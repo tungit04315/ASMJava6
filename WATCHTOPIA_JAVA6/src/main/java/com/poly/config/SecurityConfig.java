8b8777ca122ef9332ead618033130bc7d00659ed
tuonght <tuongho881@gmail.com>
2023-08-02 10:38:33 +0700

    dòng 74 security

--- WATCHTOPIA_JAVA6/src/main/java/com/poly/config/SecurityConfig.java
+++ WATCHTOPIA_JAVA6/src/main/java/com/poly/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		
 		http.authorizeRequests()
-		.antMatchers("/order/**").authenticated()
+		.antMatchers("home/profile","home/order").authenticated()
 		.antMatchers("/admin/**").hasAnyRole("STAF", "DIRE")
 		.antMatchers("/rest/authorities").hasRole("DIRE")
 		.anyRequest().permitAll();
