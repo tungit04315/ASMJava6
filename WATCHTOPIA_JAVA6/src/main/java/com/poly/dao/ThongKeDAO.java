f5d55409d7a7ff712280b072b2dffe1d0fb6d30b
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-08-12 11:20:06 +0700

    edit profile

--- WATCHTOPIA_JAVA6/src/main/java/com/poly/dao/ThongKeDAO.java
+++ WATCHTOPIA_JAVA6/src/main/java/com/poly/dao/ThongKeDAO.java
@@ -13,10 +13,17 @@ public interface ThongKeDAO  extends JpaRepository<ThongKe, String>{
 //			+ "inner join product p on d.product_id = p.product_id \r\n"
 //			+ "group by p.product_name, p.product_price")
 	
-	@Query("select new ThongKe(p.product_name, p.product_price, sum(d.quantity)) from OrderDetail d inner join Products p on d.product.product_id = p.product_id group by p.product_name, p.product_price")
+	//@Query("select new ThongKe(p.product_name, p.product_price, sum(d.quantity)) from OrderDetail d inner join Products p on d.product.product_id = p.product_id group by p.product_name, p.product_price")
+	@Query("select new ThongKe(p.product_name, p.product_price, sum(d.quantity)) \r\n"
+			+ "from OrderDetail d \r\n"
+			+ "inner join Products p \r\n"
+			+ "on d.product.product_id = p.product_id \r\n"
+			+ "inner join Order r on r.orders_id = d.order.orders_id\r\n"
+			+ "where r.status.status_id = 3\r\n"
+			+ "group by p.product_name, p.product_price")
 	List<ThongKe> getListTK();
 	
-	@Query("select count(o) from Order o")
+	@Query("select count(o) from Order o where o.status.status_id = 3")
 	Long getLuotMua();
 	
 	
